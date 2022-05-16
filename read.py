982fd4446e4a12fdf16f0d8ba0407a34762d8eff
Rafael450 <rafaelgamer450@gmail.com>
2022-05-16 16:21:18 -0300

    added scheduled tweets

--- read.py
+++ read.py
@@ -3,13 +3,10 @@ import openpyxl
 
 def read_meal(meal):
     #considerar meal = 1 para almoço e meal = 2 para janta
-    
     path = ('.\cardapio.xlsx')
     wb = openpyxl.load_workbook(path)
-    sheet = wb.active
-    
+    sheet = wb.active  
     day = datetime.datetime.today().weekday()
-    
     proteina = sheet.cell(column=meal*2, row=day+3).value
     massa = sheet.cell(column=meal*2+1, row=day+3).value
     
--- run.py
+++ run.py
@@ -1,8 +1,24 @@
 import sys
-from flask import Flask
+import config
+from flask import Flask, request
+from flask_apscheduler import APScheduler
+import create
+import json
 
 app = Flask(__name__)
+scheduler = APScheduler()
 
+@app.route('/upload', methods = ['POST'])
+def upload_file():
+    input_password = request.get_json()['password']
+    if input_password == config.PASSWORD:
+        f = request.files['file']
+        f.save('cardapio.xlsx')
+        return json.dumps({'message':'File uploaded successfully','status':200}),200 
+    else:
+        return json.dumps({'message':'Incorrect password','status':401}),401 
 
 if __name__ == '__main__':
+    scheduler.add_job(id = 'Scheduled Task', func=create.time_checker, trigger="interval", seconds=3)
+    scheduler.start()
     app.run(host='localhost', port=8080, debug=True)
